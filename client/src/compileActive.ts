279f01f6b1764cf50a89db682aa3d1bc80eb57bb
rocky <rocky.bernstein@consensys.net>
2019-06-10 15:10:39 -0400

    solc compilable ...
    shows diagnostics
    saves ast for later LSP use

--- client/src/compileActive.ts
+++ client/src/compileActive.ts
@@ -1,36 +1,60 @@
-"use strict";
 import * as path from "path";
-import { window, workspace, DiagnosticCollection } from "vscode";
-// import { compile } from "solc-lsp/out";
+import { window, /*workspace,*/
+         Diagnostic,
+         DiagnosticCollection, Uri } from "vscode";
+import { compile } from "solc-lsp";
+import { solcErrToDiagnostic } from "./diagnostics";
 
 export function compileActiveContract(diagnosticCollection: DiagnosticCollection) {
-    const editor = window.activeTextEditor;
 
+  const editor = window.activeTextEditor;
+  if (!editor) {
+    return; // We need something open
+  }
+
+  const fileName = editor.document.fileName;
+  if (path.extname(fileName) !== ".sol") {
+    window.showWarningMessage(`{filename} not a solidity file (*.sol)`);
+    return;
+  }
+
+  /*
+  // Check if is folder, if not stop we need to output to a bin folder on rootPath
+  if (workspace.rootPath === undefined) {
+  window.showWarningMessage("Please open a folder in Visual Studio Code as a workspace");
+  return;
+  }
+  */
+
+  const compiled = compile(editor.document.getText(), fileName, {});
+  if (compiled.errors) {
+    const uri = Uri.file(fileName);
+    diagnosticCollection.clear();
+    const diagnostics: Array<Diagnostic> = [];
+    for (const compiledError of compiled.errors) {
+      const diagnostic = solcErrToDiagnostic(compiledError);
+      diagnostics.push(diagnostic);
+      console.log(compiledError.formattedMessage);
+    }
+    diagnosticCollection.set(uri, diagnostics);
+  } else {
+    console.log("compileActiveContract suceeded");
+  }
+  return;
+
+}
+
+export function gotoDefinition() {
+    const editor = window.activeTextEditor;
     if (!editor) {
         return; // We need something open
     }
 
-    if (path.extname(editor.document.fileName) !== ".sol") {
-        window.showWarningMessage("This not a solidity file (*.sol)");
+    const fileName = editor.document.fileName;
+    if (path.extname(fileName) !== ".sol") {
+        window.showWarningMessage(`{filename} not a solidity file (*.sol)`);
         return;
     }
 
-    // Check if is folder, if not stop we need to output to a bin folder on rootPath
-    if (workspace.rootPath === undefined) {
-        window.showWarningMessage("Please open a folder in Visual Studio Code as a workspace");
-        return;
-    }
-
-    // const compiled = compile(editor.document.getText(), {});
-    // if (!compiled.contracts) {
-    //   if (compiled.errors) {
-    //     for (const compiledError of compiled.errors) {
-    //       console.log(compiledError.formattedMessage);
-    //     }
-    //   }
-    // }
-
-    console.log("compileActiveContract to be completed later...");
-    return;
-
+    console.log("gotoDefinition to be filled in");
 }
--- client/src/extension.ts
+++ client/src/extension.ts
@@ -1,11 +1,10 @@
-"use strict";
 import * as path from "path";
 
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 
-import { commands, workspace, WorkspaceFolder,
-         DiagnosticCollection, ExtensionContext } from "vscode";
+import { commands, /* workspace, WorkspaceFolder, */
+         ExtensionContext } from "vscode";
 import * as vscode from "vscode";
 
 import {
