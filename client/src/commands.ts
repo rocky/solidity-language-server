f112822c5fbb79445885b502f1b64831756b17c5
rocky <rocky.bernstein@consensys.net>
2019-06-14 22:21:08 -0400

    Add getTypeDefinition and gotoDefinition
    Done most minimally.

--- client/src/commands.ts
+++ client/src/commands.ts
@@ -1,11 +1,27 @@
 import * as path from "path";
-import { window, /*workspace,*/
-         Diagnostic,
-         DiagnosticCollection, Uri } from "vscode";
+import {
+  window, /*workspace,*/
+  Diagnostic,
+  DiagnosticCollection,
+  Range,
+  Selection,
+  Uri
+} from "vscode";
 import * as solcLsp from "solc-lsp";
 import { solcErrToDiagnostic } from "./diagnostics";
 
 const lspMgr = new solcLsp.LspManager();
+
+function selectionToRange(selection: Selection): Range {
+  // FIXME put into a function
+  return new Range(
+    selection.start.line + 1,
+    selection.start.character,
+    selection.end.line + 1,
+    selection.end.character
+  );
+}
+
 
 export function compileActiveContract(diagnosticCollection: DiagnosticCollection) {
 
@@ -56,6 +72,37 @@ export function gotoDefinition() {
     return;
   }
 
-  lspMgr.gotoDefinition(fileName, editor.selection);
-  console.log("gotoDefinition to be filled in");
+  const lineBreakOffsets = lspMgr.fileInfo[fileName].sourceMapping.lineBreaks;
+  const range = selectionToRange(editor.selection);
+  const srcPosition = solcLsp.rangeToLspPosition(range, lineBreakOffsets);
+  const defNode = lspMgr.gotoDefinition(fileName, srcPosition);
+  if (defNode) {
+    const defPosition = defNode.src;
+    window.showInformationMessage(`Definition at ${defPosition}`);
+  }
+}
+
+export function getTypeDefinition() {
+  const editor = window.activeTextEditor;
+  if (!editor) {
+    return; // We need something open
+  }
+
+  const fileName = editor.document.fileName;
+  if (path.extname(fileName) !== ".sol") {
+    window.showWarningMessage(`${fileName} not a solidity file (*.sol)`);
+    return;
+  }
+
+  const lineBreakOffsets = lspMgr.fileInfo[fileName].sourceMapping.lineBreaks;
+  const range = selectionToRange(editor.selection);
+  const srcPosition = solcLsp.rangeToLspPosition(range, lineBreakOffsets);
+  const selectedNode = lspMgr.gotoTypeDefinition(fileName, srcPosition);
+  if (selectedNode) {
+    const typeName = selectedNode.typeDescriptions.typeString;
+    const mess = "name" in selectedNode ?
+      `${selectedNode.name} has type: ${typeName}` :
+      ` type: ${typeName}`;
+    window.showInformationMessage(mess);
+  }
 }
