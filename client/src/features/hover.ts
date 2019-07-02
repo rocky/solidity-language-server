a023de7f7d4f85b4f8e2962e7350245b9e4280c7
rocky <rocky.bernstein@consensys.net>
2019-07-02 05:38:29 -0400

    ...And hover starts working!

--- client/src/features/hover.ts
+++ client/src/features/hover.ts
@@ -1,7 +1,6 @@
 /* A placeholder for a hover provider for Solidity */
 
 import * as vscode from "vscode";
-import * as util from "util";
 import { LspManager } from "solc-lsp";
 export function registerSolidityHover(lspMgr: LspManager) {
   vscode.languages.registerHoverProvider("solidity",
@@ -9,12 +8,25 @@ export function registerSolidityHover(lspMgr: LspManager) {
       provideHover(document: vscode.TextDocument, position: vscode.Position,
         token: vscode.CancellationToken) {
         if (document.uri.fsPath in lspMgr.fileInfo) {
-          /* FIXME: look up postion in lspMgr */
-          const mess = `I am a hover for ${document}, ${util.inspect(position)}, ${util.inspect(token)}`;
-          return new vscode.Hover(mess);
-        } else {
-          return undefined;
+          const info = lspMgr.fileInfo[document.uri.fsPath];
+          const staticInfo = info.staticInfo;
+          const solcOffset = info.sourceMapping.offsetFromLineColPosition(position);
+          const node = staticInfo.offsetToAstNode(solcOffset);
+          let mess: string;
+          if (node) {
+            token;
+            if (node.typeName && node.typeName.name) {
+              mess = `${node.nodeType} node at ${node.src} has type ${node.typeName.name}`;
+            } else if (node.typeDescriptions && node.typeDescriptions.typeString) {
+              mess = `${node.nodeType} node at ${node.src} has is type description ${node.typeDescriptions.typeString}`;
+            } else {
+              mess = `node at ${node.src} is ${node.nodeType}`;
+              // console.log(util.inspect(node));
+            }
+            return new vscode.Hover(mess);
+          }
         }
+        return undefined;
       }
     });
 }
